61b4dc92f783a9f46f7ba091678b8b77aa269712
João Ferrari <joaovictorbf.jvf@gmail.com>
2023-04-13 15:40:53 -0300

    ajustes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+
+const YourMoviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+
+
 const Home = () => {
-    return <div>Home</div>;
+    const [bestMovies, setBestMovies] = useState([])
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+        setBestMovies(data.results);
+      };
+
+      useEffect(() => {
+        const topRatedUrl = `${YourMoviesURL}top_rated?${apiKey}`;
+        console.log(topRatedUrl);
+        getTopRatedMovies(topRatedUrl);
+      }, []);
+
+      console.log(bestMovies);
+
+
+    return (
+    <div>
+        {bestMovies &&
+        bestMovies.map((movie) => <p>{movie.title}</p>)}
+    </div>
+    );
 };
 
 export default Home;
